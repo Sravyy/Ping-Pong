c92855e3245fe767761391b7f4aa1933fe1927b4
sravanthi velaga <svelaga0412@gmail.com>
2017-09-22 11:33:04 -0700

    Add ul to capture the output

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
 
-$(function(){
+
 
   function pingPong(userInput) {
     var result = [];
     for (var i=3; i <= userInput ; i++){
-      if(i) <= 0){
+      if(i <= 0){
         alert("please enter numbers between 1 and 1000")
       }
-      if( i % 15 ) {
+      if( i % 15 == 0) {
         result.push("PingPong");
       } else if ( i % 3 == 0 ){
         result.push("Ping");
       } else if( i % 5 == 0 ) {
         result.push("Ping");
       } else {
-        result.pish(i);
+        result.push(i);
       }
     }
     return result
   };
 
-
+$(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
 
     var userInput = parseInt($("input#ping").val());
     var result = pingPong(userInput);
-    $("#output").text(result);
+    $("ul#showOutput").prepend("<li>"+ result+ "</li>");
     $("#result").show();
   });
 });
